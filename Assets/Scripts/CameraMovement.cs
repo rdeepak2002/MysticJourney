096ee6485f81feb4f7f973f0e22b7ad2a5db3dab
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-22 23:08:19 -0700

    wow

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
 
 	public Transform target;
-	public float smoothing; 
+	public float smoothing;
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public int health;
+	public string enemyName;
+	public int baseAttack;
+	public float moveSpeed;
+	public float battleRadius;
+	public float attackRadius;
+	public Transform target;
+	public AudioSource audioSource;
+	public AudioClip bossMusic;
+	public CameraMovement cameraMovementScript;
+	public bool preparedForBattle = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	protected void checkDistance() {
+        if (!preparedForBattle && Vector3.Distance(target.position, transform.position) <= battleRadius) {
+			prepareForBattle();
+        }
+	}
+
+	protected void prepareForBattle() {
+		audioSource.clip = bossMusic;
+		audioSource.Play();
+		preparedForBattle = true;
+		cameraMovementScript.target = transform;
+    }
+
+	protected void moveTowards() {
+		transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+	}
+}
