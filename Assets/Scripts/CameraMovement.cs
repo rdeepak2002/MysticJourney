17ced4c1f3a460b1f41de04148ffc4012c5470c8
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-22 19:41:26 -0700

    added smooth camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(transform.position != target.position) {
+			Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+			transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+			
+		}
+	}
+}
