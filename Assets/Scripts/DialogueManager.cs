193d63871c57b57fa2aeb6a6a9e0db30786c09dd
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-23 00:26:24 -0700

    added animated text

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -7,11 +7,14 @@ public class DialogueManager : MonoBehaviour {
 
 	public GameObject dBox;
 	public Text dText;
+	public Text hint;
 
 	public bool dialogueActive;
 
 	// Use this for initialization
 	void Start () {
+		hint.text = "";
+
 		if (!dialogueActive) {
 		    dBox.SetActive(false);
 		}
@@ -19,16 +22,30 @@ public class DialogueManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (dialogueActive && Input.GetKeyDown(KeyCode.Space)) {
+		if (hint.text != "" && dialogueActive && Input.GetKeyDown(KeyCode.Space)) {
 			dBox.SetActive(false);
 			dialogueActive = false;
 			dText.text = "blank";
+			hint.text = "";
 		}
 	}
 
 	public void ShowDialogueBox(string dialogue) {
 		dialogueActive = true;
 		dBox.SetActive(true);
-		dText.text = dialogue;
+		dText.text = "";
+
+		StartCoroutine(writer(dialogue));
     }
+
+	IEnumerator writer(string dialogue)
+	{
+		foreach (var letter in dialogue.ToCharArray())
+		{
+			dText.text += letter;
+			yield return new WaitForSeconds(.07f);
+		}
+
+		hint.text = "Press Space to Continue";
+	}
 }
