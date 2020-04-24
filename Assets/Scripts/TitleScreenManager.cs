d9c39fb6f78e94278c39c04232acf5e3dfdc7e17
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-23 23:12:17 -0700

    added interactive title screen

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour {
+
+	public void changeScene(string sceneName) {
+		Debug.Log("??");
+		SceneManager.LoadScene(sceneName);
+	}
+}
