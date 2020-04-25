2178d998a67878ec54b538608532b0f6ec1c6482
rdeepak2002 <rdeepak2002@gmail.com>
2020-04-24 23:35:26 -0700

    animated title screen

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour {
+	public GameObject fadeInPanel;
+
+	public void Awake() {
+		GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+		Destroy(panel, 5);
+    }
+}
--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreenManager : MonoBehaviour {
+	public string gameSceneName;
+
+	public GameObject fadeInPanel;
+	public GameObject fadeOutPanel;
+
+    public AudioSource music;
+
+	public void Awake()
+	{
+		GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+		Destroy(panel, 4);
+	}
 
 	public void changeScene(string sceneName) {
-		Debug.Log("??");
-		SceneManager.LoadScene(sceneName);
+        music.mute = true;
+        GameObject panel = Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity) as GameObject;
+		Invoke("MyLoadingFunction", 5f);
+	}
+
+	void MyLoadingFunction()
+	{
+		SceneManager.LoadScene(gameSceneName);
 	}
 }
