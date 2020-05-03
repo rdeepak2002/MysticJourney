fea8481e673f28d4963b7449d5dc8465292f7117
rdeepak2002 <rdeepak2002@gmail.com>
2020-05-02 17:54:40 -0700

    implemented basic a*

--- Assets/Scripts/KyubueMovement.cs
+++ Assets/Scripts/KyubueMovement.cs
@@ -8,10 +8,9 @@ public class KyubueMovement : MonoBehaviour
     public float distanceRadius;
     public float minSpeed;
     public float maxSpeed;
-    public GameObject target;
+    //public GameObject target;
 
     private Vector3 change;
-    private Rigidbody2D myRigidBody;
     private Animator animator;
 
     private float currentSpeed;
@@ -19,31 +18,28 @@ public class KyubueMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start() {
         animator = GetComponent<Animator>();
-        myRigidBody = GetComponent<Rigidbody2D>();
 
         currentSpeed = minSpeed;
     }
 
     // Update is called once per frame
     void Update() {
-        change = Vector3.zero;
+        //change = Vector3.zero;
 
-        UpdateAnimationAndMove();
+        //UpdateAnimationAndMove();
     }
 
-    void UpdateAnimationAndMove()
-    {
-
-
-        if (change != Vector3.zero)
-        {
-            animator.SetFloat("moveX", change.x);
-            animator.SetFloat("moveY", change.y);
-            animator.SetBool("moving", true);
-        }
-        else
-        {
-            animator.SetBool("moving", false);
-        }
-    }
+    //void UpdateAnimationAndMove()
+    //{
+    //    if (change != Vector3.zero)
+    //    {
+    //        animator.SetFloat("moveX", change.x);
+    //        animator.SetFloat("moveY", change.y);
+    //        animator.SetBool("moving", true);
+    //    }
+    //    else
+    //    {
+    //        animator.SetBool("moving", false);
+    //    }
+    //}
 }
